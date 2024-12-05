9daa32c7f09532d4087f1d97c2bff5da91401c51
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2024-12-05 18:44:07 +0000

    Gem Sandpaper and melting ore powders.

--- kubejs/server_scripts/quick_recipes.js
+++ kubejs/server_scripts/quick_recipes.js
@@ -4,3 +4,15 @@
 
 
 
+//I cant get this working in json
+ServerEvents.recipes(event =>{
+    event.shapeless(
+        Item.of('createaddition:diamond_grit_sandpaper', "{Damage:512,RepairCost:0,display:{Name:'{\"text\":\"Gem Sandpaper\"}'}}"),
+        [
+            "tfc:glue",
+            "minecraft:paper",
+            "#tfc:gem_powders"
+        ]
+    )
+})
+
