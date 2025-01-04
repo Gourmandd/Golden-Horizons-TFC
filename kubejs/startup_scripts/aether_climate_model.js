7efc5043c3207a8b4fc74ae03e9b34a0449c4647
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-01-04 13:56:11 +0000

    Attempt at Aether Climate Model

---
+++ kubejs/startup_scripts/aether_climate_model.js
@@ -0,0 +1,25 @@
+
+/*
+TFCEvents.registerClimateModel(event => {
+    event.registerClimateModel("kubejs:aether", model => {
+        model.setCurrentTemperatureCalculation((level, pos, calendarTicks, daysInMonth) => {
+            return pos.get * -0.2
+        })
+        model.setAverageTemperatureCalculation((level, pos) => {
+            return pos.get * -0.2
+        })
+        model.setAverageRainfallCalculation((level, pos) => {
+            return 100
+        })
+        model.setAirFog((level, pos, calendarTicks) => {
+            return 0.25
+        })
+        model.setWaterFog((level, pos, calendarTicks) => {
+            return 0.25
+        })
+        model.setWindVector((block, calendarTicks) => {
+            return event.newVec2(1, 1)
+        })
+    })
+})
+*/
