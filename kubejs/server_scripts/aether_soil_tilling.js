ffddcf6814af9bd5f92c3270a90f3e5dcc269852
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-01-18 16:56:50 +0000

    Fixed Aether Soil Tilling

--- kubejs/server_scripts/aether_soil_tilling.js
+++ kubejs/server_scripts/aether_soil_tilling.js
@@ -3,33 +3,47 @@ BlockEvents.rightClicked(event =>{
     //event.player.tell(event.player.mainHandItem.id)
     //event.player.tell(event.getBlock().getId())
 
-    let aether_dirt = [
+    // check for dirt block
+    let aetherDirt = [
         "aether:aether_dirt", "aether:aether_grass_block",
         "aether:enchanted_aether_grass_block", 
         "aether_redux:avelium", "aether:aether_dirt_path", 
         "deep_aether:golden_heights_grass_block", "deep_aether:golden_heights_dirt_path",
         "aether:aether_farmland"
     ]
-    let hoes = Ingredient.of("#minecraft:hoes").stacks
-    let hoe_list = []
+
+    if (aetherDirt.indexOf(event.getBlock().getId()) == -1){
+        console.log("test 1")
+        return
+    }
+
+    // check for hoe
+    let hoes = Ingredient.of("#minecraft:hoes").itemIds
+    let hoeList = []
     let pos = event.getBlock().getPos()
 
     hoes.forEach(item => {
-        hoe_list.push(item)
+        hoeList.push(item)
     })
 
-
-    if (aether_dirt.indexOf(event.getBlock().getId()) == -1){
+    if (hoeList.indexOf(event.player.mainHandItem.id) == -1){
+        //console.log("test 2")
         return
     }
 
-    if (hoe_list.indexOf(event.player.mainHandItem.id) == -1){
+    // check above block
+    let aboveBlock = event.getLevel().getBlock(pos.getX(), pos.getY() + 1, pos.getZ())
+
+    let blockWhitelist = [
+        "minecraft:air", "minecraft:cave_air"
+    ]
+
+    if (blockWhitelist.indexOf(aboveBlock) == -1){
+        console.log("test 3 " + aboveBlock)
         return
     }
 
-    if (event.getLevel().getBlock(pos.getX(), pos.getY() + 1, pos.getZ()) == "minecraft:air"){
-        event.getBlock().set("kubejs:dirt/aether_farmland")
-    }
-
+    // if all passed then set the block
+    event.getBlock().set("kubejs:dirt/aether_farmland")
 
 })
