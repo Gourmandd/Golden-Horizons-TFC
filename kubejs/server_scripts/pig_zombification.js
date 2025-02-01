3c63eca0435a92daf315487b57064a7c729865e8
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-02-01 13:40:30 +0000

    Vakyrum generation and many datapack fixes. To catch up repo to development.

---
+++ kubejs/server_scripts/pig_zombification.js
@@ -0,0 +1,41 @@
+
+// Idea inspired by @Talerzykmoth in https://discord.com/channels/432522930610765835/432522930610765837/1330472744390885387
+
+ItemEvents.entityInteracted(event =>{
+    
+    let Target = event.getTarget()
+
+    let MobWhitelist = [
+        "tfc:pig",
+        "minecraft:pig",
+        "aether:phyg"
+    ]
+    if (MobWhitelist.indexOf(Target.type) == -1){
+        //event.player.tell("not pig is " + Target.type ) //debug
+        return
+    }
+
+    if (event.getItem().id != "minecraft:rotten_flesh"){
+        //event.player.tell("not right item is " + event.getItem().id) //debug
+        return
+    }
+
+    let NewEntity = event.level.createEntity("minecraft:zoglin")
+    let Lightning = event.level.createEntity("minecraft:lightning_bolt")
+
+    NewEntity.x+= Target.x
+    NewEntity.y+= Target.y
+    NewEntity.z+= Target.z
+
+    Lightning.x+= Target.x
+    Lightning.y+= Target.y
+    Lightning.z+= Target.z
+
+    Target.kill()
+    NewEntity.spawn()
+    Lightning.spawn()
+
+    event.getPlayer().playNotifySound("ambient.cave", "blocks", 1, 1)
+    event.player.tell("That was a mistake...")
+
+})
