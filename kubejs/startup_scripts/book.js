b7dd7990d9a1466dd1d5f2d82ac3182d2fbc576f
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-03-05 21:08:01 +0000

    Remove wax tablet item

--- kubejs/startup_scripts/book.js
+++
@@ -1,7 +0,0 @@
-const $WritableBookItem = Java.loadClass("net.minecraft.world.item.WritableBookItem")
-
-StartupEvents.registry("item", event =>{
-
-    event.createCustom("wax_tablet", () => new $WritableBookItem(new $ItemProperties().stacksTo(1)))
-
-})
