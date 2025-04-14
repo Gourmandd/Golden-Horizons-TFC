7bed85dcb779baf9a7d5ee7633ec65c4db45c38f
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-04-14 16:42:05 +0100

    Added greeting message when the player joins a world.

---
+++ kubejs/server_scripts/guidebook_reminder.js
@@ -0,0 +1,4 @@
+
+PlayerEvents.loggedIn(event =>{
+    event.player.tell("Hello " + event.player.username + "! The TerraFirmaCraft Guidebook found in the inventory menu, explains many of its game mechanics!")
+})
