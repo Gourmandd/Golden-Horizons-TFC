c35bb54ace6bd033e40728f4be7567303722b6ed
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-04-15 14:16:31 +0100

    Misc fixes.

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -6,17 +6,20 @@ ItemEvents.tooltip(event =>{
         "minecraft:furnace",
         "minecraft:blast_furnace",
         "minecraft:grindstone",
-        "minecraft:anvil",
-        "minecraft:damaged_anvil",
-        "minecraft:chipped_anvil",
         "minecraft:brewing_stand",
         "minecraft:beacon",
         "minecraft:enchanting_table",
-        "minecraft:fletching_table",
-        "aether:altar",
-        "aether:freezer"
+        "minecraft:fletching_table"
     ].forEach(item =>{
         event.add(item, Text.translate("modpack.item_functionality").color(0x9D2829))
     })
 
+    [
+        "minecraft:anvil",
+        "minecraft:damaged_anvil",
+        "minecraft:chipped_anvil"
+    ].forEach(item =>{
+        event.add(item, Text.translate("modpack.item_changed_functionality"))
+    })
+    
 })
