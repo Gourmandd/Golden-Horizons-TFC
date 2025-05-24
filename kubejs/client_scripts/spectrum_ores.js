2dd5621dd3a50ef4887fef5c429c723181d5cc60
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-05-24 08:51:40 +0100

    Updated Ore assets.

--- kubejs/client_scripts/spectrum_ores.js
+++ kubejs/client_scripts/spectrum_ores.js
@@ -12,10 +12,11 @@ ClientEvents.lang("en_us", event =>{
         "shimmerstone": "Something Shiny?",
         "azurite": "Something?",
         "paltaeria": "Something?",
-        "stratine": "Something Dense"
+        "stratine": "Something Dense",
+        "malachite": "Something Powerful"
     }
 
-    global.SPECTRUM_OVERWORLD_ORES.forEach(ore => {
+    Object.keys(OVERWORLD_SPECTRUM_PROSPECTING).forEach(ore => {
 
         global.ROCK_TYPES.forEach(rockType =>{
 
@@ -27,16 +28,71 @@ ClientEvents.lang("en_us", event =>{
 
 ClientEvents.highPriorityAssets(event =>{
 
+    function generateOreBlockModel(location, all, overlay){
+        event.add(location, {
+            "parent": "tfc:block/ore",
+            "textures": {
+                "all": all,
+                "overlay": overlay
+            }
+        })
+    }
+
+    const DEEPER_DOWN_ROCK_TYPES = {
+        "argillite": "minecraft:block/stone", //classic stone
+        "nephelinite": "minecraft:block/deepslate", //deepslate
+        "blackslag": "spectrum:block/blackslag", //blackslag
+        "travertine": "minecraft:block/dripstone_block", //dripstone
+        "picrite_basalt": "spectrum:block/basal_marble" //basal marble
+    }
+
+    const ORE_BLOCK_GRADES = [
+        "normal",
+        "poor",
+        "rich",
+    ]
+
     global.SPECTRUM_OVERWORLD_ORES.forEach(ore => {
 
         global.ROCK_TYPES.forEach(rockType =>{
+            generateOreBlockModel("kubejs:models/block/ore/" + ore + "/" + rockType, "tfc:block/rock/raw/" + rockType, "kubejs:block/spectrum/overworld_ore/" + ore)
+        })
 
-            event.add("kubejs:models/block/ore/" + ore + "/" + rockType, {
-                "parent": "tfc:block/ore",
-                "textures": {
-                    "all": "tfc:block/rock/raw/" + rockType,
-                    "overlay": "kubejs:block/spectrum/overworld_ore/" + ore
-                }
+        Object.keys(DEEPER_DOWN_ROCK_TYPES).forEach(rockType =>{
+            generateOreBlockModel("kubejs:models/block/ore/" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "kubejs:block/spectrum/overworld_ore/" + ore)
+        })
+    })
+
+
+    global.SPECTRUM_DEEPER_DOWN_ORES.forEach(ore => {
+
+        global.ROCK_TYPES.forEach(rockType =>{
+            generateOreBlockModel("kubejs:models/block/ore/" + ore + "/" + rockType, "tfc:block/rock/raw/" + rockType, "kubejs:block/spectrum/overworld_ore/" + ore)
+        })
+
+        Object.keys(DEEPER_DOWN_ROCK_TYPES).forEach(rockType =>{
+            generateOreBlockModel("kubejs:models/block/ore/" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "kubejs:block/spectrum/overworld_ore/" + ore)
+        })
+    })
+    
+    Object.keys(DEEPER_DOWN_ROCK_TYPES).forEach(rockType =>{
+        global.TFC_MINERALS.forEach(ore =>{
+            generateOreBlockModel("kubejs:models/block/ore/" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "tfc:block/ore/" + ore)
+        })
+
+        global.TFC_GEMS.forEach(ore =>{
+            generateOreBlockModel("kubejs:models/block/ore/" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "tfc:block/ore/" + ore)
+        })
+
+        global.TFC_ORES.forEach(ore =>{
+            generateOreBlockModel("kubejs:models/block/ore/rich_" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "tfc:block/ore/rich/" + ore)
+            generateOreBlockModel("kubejs:models/block/ore/poor_" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "tfc:block/ore/poor/" + ore)
+            generateOreBlockModel("kubejs:models/block/ore/normal_" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "tfc:block/ore/normal_" + ore)
+        })
+
+        global.FIRMALIFE_ORES.forEach(ore =>{
+            ORE_BLOCK_GRADES.forEach(grade =>{
+                generateOreBlockModel("kubejs:models/block/ore/" + grade + "_" + ore + "/" + rockType, DEEPER_DOWN_ROCK_TYPES[rockType], "firmalife:block/ore/" + grade + "_" + ore)
             })
         })
     })
