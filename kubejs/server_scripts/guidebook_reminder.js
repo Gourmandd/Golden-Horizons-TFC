afee75135f6d090cd9a1c2c77b9f27ff0cad75d2
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-06-27 16:45:07 +0100

    Various Small Tweaks and fixes.

--- kubejs/server_scripts/guidebook_reminder.js
+++
@@ -1,4 +0,0 @@
-
-PlayerEvents.loggedIn(event =>{
-    event.player.tell("Hello " + event.player.username + "! The TerraFirmaCraft Guidebook found in the inventory menu, explains many of its game mechanics!")
-})
