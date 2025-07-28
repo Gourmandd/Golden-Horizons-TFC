ffdac333bfb530416792e465aad6ff82672c4e92
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-07-28 08:45:05 +0100

    Deeper Down mushroom twigs

--- kubejs/startup_scripts/spectrum_wood.js
+++ kubejs/startup_scripts/spectrum_wood.js
@@ -18,3 +18,74 @@ StartupEvents.registry("item", event => {
 
     })
 })
+
+
+
+StartupEvents.registry("block", event => {
+
+    global.DYE_COLOURS.forEach(colour => {
+
+        event.create(`wood/support/${colour}`, "tfc:support")
+            .displayName(`${nameProcessing(colour)} Support`)
+            .tagBlock("minecraft:mineable/axe")
+            .tagBlock("tfc:support_beams")
+            .tagItem("tfc:support_beams")
+            .model(`kubejs:block/wood/support/${colour}_vertical`)
+            .connectionModel(`kubejs:block/wood/support/${colour}_connection`)
+            .horizontal(block => {
+                 block.displayName(`Horizontal ${nameProcessing(colour)} Support`)
+            })
+    })
+
+    global.SPECTRUM_WOODS.forEach(wood => {
+
+        if (global.NOXWOODS.indexOf(wood) > -1){
+            
+            event.create(`wood/twig/${wood}_noxwood`, "tfc:ground_cover")
+                .displayName(`${nameProcessing(wood)} Noxwood Twig`)
+                .twig()
+                .tagItem("tfc:can_be_lit_on_torch")
+                .tagItem("tfc:twigs")
+                .tagItem("forge:rods/wooden")
+                .tagBlock("minecraft:mineable/axe")
+                .tagBlock("tfc:twigs")
+                .tagBlock("tfc:can_be_snow_piled")
+            
+            event.create(`wood/support/${wood}_noxwood`, "tfc:support")
+                .displayName(`${nameProcessing(wood)} Noxwood Support`)
+                .tagBlock("minecraft:mineable/axe")
+                .tagBlock("tfc:support_beams")
+                .tagItem("tfc:support_beams")
+                .model(`kubejs:block/wood/support/${wood}_noxcap_vertical`)
+                .connectionModel(`kubejs:block/wood/support/${wood}_noxcap_connection`)
+                .horizontal(block => {
+                    block.displayName(`Horizontal ${nameProcessing(wood)} Noxwood Support`)
+                    block.model(`kubejs:block/wood/support/${wood}_noxcap_horizontal`)
+                })
+
+        } else {
+
+            event.create(`wood/twig/${wood}`, "tfc:ground_cover")
+                .displayName(`${nameProcessing(wood)} Twig`)
+                .twig()
+                .tagItem("tfc:can_be_lit_on_torch")
+                .tagItem("tfc:twigs")
+                .tagItem("forge:rods/wooden")
+                .tagBlock("minecraft:mineable/axe")
+                .tagBlock("tfc:twigs")
+                .tagBlock("tfc:can_be_snow_piled")
+
+            event.create(`wood/support/${wood}`, "tfc:support")
+                .displayName(`${nameProcessing(wood)} Support`)
+                .tagBlock("minecraft:mineable/axe")
+                .tagBlock("tfc:support_beams")
+                .tagItem("tfc:support_beams")
+                .model(`kubejs:block/wood/support/${wood}_vertical`)
+                .connectionModel(`kubejs:block/wood/support/${wood}_connection`)
+                .horizontal( block => {
+                    block.displayName(`Horizontal ${nameProcessing(wood)} Support`)
+                })
+        }
+
+    })
+})
