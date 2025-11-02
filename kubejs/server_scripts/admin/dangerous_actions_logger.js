8ad2d3969d37abd583a70ecc085d5bf32877f036
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-11-02 17:40:16 +0000

    Made some server tools.

---
+++ kubejs/server_scripts/admin/dangerous_actions_logger.js
@@ -0,0 +1,22 @@
+
+const DO_ACTION_LOGGING = false
+
+BlockEvents.placed(event => {
+
+    if (!DO_ACTION_LOGGING){
+        return
+    }
+
+    let blockBlacklist = [
+        "tfc:powderkeg",
+        "pastel:ruin",
+        "pastel:failing"
+    ]
+
+    if (blockBlacklist.indexOf(event.block.id) < 0){
+        return
+    }
+
+    console.log(`Player: ${event.player.username}(${event.player.getUuid().toString()}) placed ${event.block.id} at ${event.block.pos.toString()}`)
+
+})
--- kubejs/server_scripts/admin/spawn_protection.js
+++ kubejs/server_scripts/admin/spawn_protection.js
@@ -1,17 +1,24 @@
-/*
-let protectionPoints = [
-    {"x": 0 , "y": 0}
-]
 
-let blockBlacklist = [
-    "minecraft:bedrock"
-]
+
+const DO_PROTECTION = false
 
 function getDistanceBetweenVectors(x1, y1, x2, y2){
-    return Math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    return Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1))
 }
 
 BlockEvents.placed(event =>{
+
+    if (!DO_PROTECTION){
+        return
+    }
+
+    let protectionPoints = [
+        {"x": 0 , "y": 0, "range": 5000}
+    ]
+
+    let blockBlacklist = [
+        "minecraft:bedrock"
+    ]
 
     // return if block isnt being blacklisted
     if (blockBlacklist.indexOf(event.block.id) < 0){
@@ -19,10 +26,15 @@ BlockEvents.placed(event =>{
     }
 
     // check each point and return and cancel if its in the radius.
-    protectionPoints.forEach(vector =>{
-        if (getDistanceBetweenVectors(vector.x, vector.y, event.block.getY(), event.block.getX()) < 1000){
+    protectionPoints.forEach(point =>{
+
+        let pos = getDistanceBetweenVectors(point.x, point.y, event.block.getY(), event.block.getX())
+
+        if (pos < point.range){
+            console.log("Violation of spawn protection at: " + String(pos))
+            event.player.displayClientMessage("Spawn protection doesn't allow you to do this.", true)
             event.cancel()
             return
         }
    })
-})*/
+})
