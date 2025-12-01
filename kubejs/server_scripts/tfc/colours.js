b6ce806afaecf6903b80f3c4e3d845b36de302ab
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-12-01 17:20:03 +0000

    Restructured dye recipes.

--- kubejs/server_scripts/tfc/colours.js
+++ kubejs/server_scripts/tfc/colours.js
@@ -2,14 +2,73 @@
 // Generates recipes related to coloured items  //
 // -------------------------------------------- //
 
-ServerEvents.recipes(event =>{
-    
-    global.DYE_COLOURS.forEach(colour =>{
+ServerEvents.recipes(event => {
+
+    global.DYE_COLOURS.forEach(colour => {
 
         event.shapeless(`chalk:${colour}_chalk`, ["#tfc:glass_batches", `minecraft:${colour}_dye`])
             .id(`chalk:chalk_from_${colour}_dye`)
-        
+
         event.shapeless(`tfc:ceramic/${colour}_unfired_vessel`, ["#modpack:unfired_vessels", `minecraft:${colour}_dye`])
             .id(`tfc:crafting/ceramic/${colour}_unfired_vessel`)
+
+        event.custom({
+            "type": "create:milling",
+            "ingredients": [
+                {
+                    "tag": `c:dyes/makes_${colour}`
+                }
+            ],
+            "results": [
+                {
+                    "id": `minecraft:${colour}_dye`,
+                    "count": 1
+                }
+            ],
+            "processingTime": 50
+        }).id(`modpack:milling/dye/making_${colour}_dye`)
+
+        event.custom({
+            "type": "tfc:quern",
+            "ingredient": {
+                "tag": `c:dyes/makes_${colour}`
+            },
+            "result": {
+                "id": `minecraft:${colour}_dye`,
+                "count": 1
+            }
+        }).id(`modpack:quern/dye/making_${colour}_dye`)
+
+        event.custom({
+            "type": "tfc:pot",
+            "ingredients": [
+                {
+                    "tag": `c:dyes/makes_${colour}`
+                },
+                {
+                    "tag": `c:dyes/makes_${colour}`
+                },
+                {
+                    "tag": `c:dyes/makes_${colour}`
+                },
+                {
+                    "tag": `c:dyes/makes_${colour}`
+                },
+                {
+                    "tag": `c:dyes/makes_${colour}`
+                }
+            ],
+            "fluid_ingredient": {
+                "fluid": "minecraft:water",
+                "amount": 1000
+            },
+            "duration": 1000,
+            "temperature": 300,
+            "fluid_output": {
+                "id": `tfc:${colour}_dye`,
+                "amount": 1000
+            }
+        }).id(`modpack:pot/dye/making_${colour}_dye`)
+
     })
 })
--- kubejs/server_scripts/tfc/create/compacting.js
+++ kubejs/server_scripts/tfc/create/compacting.js
@@ -1,9 +1,9 @@
-ServerEvents.recipes(event =>{
+ServerEvents.recipes(event => {
 
     let datagen = Datagen(event).recipe()
     const ITEM = IO_TYPE.ITEM
-    
-    global.TFC_METALS.forEach(metal =>{
+
+    global.TFC_METALS.forEach(metal => {
 
         datagen.createCompacting(
             outputOf(ITEM, `tfc:metal/double_ingot/${metal}`, 1, 1),
@@ -11,47 +11,47 @@ ServerEvents.recipes(event =>{
                 inputOf(ITEM, `tfc:metal/ingot/${metal}`, 1),
                 inputOf(ITEM, `tfc:metal/ingot/${metal}`, 1),
                 inputOf(ITEM, "tfc:powder/flux", 1)
-            ] 
+            ]
         ).heatRequirement(HEAT_REQUIREMENT.HEATED).id(`modpack:compacting/double_ingot/${metal}`).generate()
 
         datagen.createCompacting(
-            outputOf(ITEM, `tfc:metal/double_sheet/${metal}`, 1, 1), 
+            outputOf(ITEM, `tfc:metal/double_sheet/${metal}`, 1, 1),
             [
                 inputOf(ITEM, `tfc:metal/sheet/${metal}`, 1),
                 inputOf(ITEM, `tfc:metal/sheet/${metal}`, 1),
                 inputOf(ITEM, "tfc:powder/flux", 1)
-            ] 
+            ]
         ).heatRequirement(HEAT_REQUIREMENT.HEATED).id(`modpack:compacting/double_sheet/${metal}`).generate()
 
-        if (metal != "wrought_iron"){
+        if (metal != "wrought_iron") {
 
             datagen.createCompacting(
-                outputOf(ITEM, `tfc:metal/sheet/${metal}`, 1, 1), 
+                outputOf(ITEM, `tfc:metal/sheet/${metal}`, 1, 1),
                 [
                     inputOf(IO_TYPE.FLUID, `tfc:metal/${metal}`, 200)
-                ] 
+                ]
             ).heatRequirement(HEAT_REQUIREMENT.HEATED).id(`modpack:compacting/sheet/${metal}`).generate()
         }
     })
 
-    global.TFC_SAND_COLOURS.forEach(colour =>{
+    global.TFC_SAND_COLOURS.forEach(colour => {
 
         datagen.createCompacting(
-            outputOf(ITEM, `tfc:raw_sandstone/${colour}`, 1, 1), 
+            outputOf(ITEM, `tfc:raw_sandstone/${colour}`, 1, 1),
             [
                 inputOf(ITEM, `tfc:sand/${colour}`, 1),
                 inputOf(ITEM, `tfc:sand/${colour}`, 1)
-            ] 
+            ]
         ).id(`modpack:compacting/sand/${colour}`).generate()
     })
 
     global.DYE_COLOURS.forEach(colour => {
 
         datagen.createCompacting(
-            outputOf(ITEM, `minecraft:${colour}_dye`, 1, 1), 
+            outputOf(ITEM, `minecraft:${colour}_dye`, 1, 1),
             [
                 inputOf(IO_TYPE.FLUID, `tfc:${colour}_dye`, 1000)
-            ] 
+            ]
         ).heatRequirement(HEAT_REQUIREMENT.HEATED).id(`modpack:compacting/dye/${colour}`).generate()
     })
 })
