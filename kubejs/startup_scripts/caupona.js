794ec102814735f6e8f4c68d0a0f68133d4e3b7e
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-12-09 21:43:47 +0000

    Work on brew mechanic.

---
+++ kubejs/startup_scripts/caupona.js
@@ -0,0 +1,14 @@
+let StewItem = Java.loadClass("com.teammoeg.caupona.item.StewItem")
+let CPBlocks = Java.loadClass("com.teammoeg.caupona.CPBlocks")
+let ItemClass = Java.loadClass("net.minecraft.world.item.Item")
+//let Lazy = Java.loadClass("net.neoforged.neoforge.common.util.Lazy")
+let BuiltInRegistries = Java.loadClass("net.minecraft.core.registries.BuiltInRegistries")
+let ResourceLocation = Java.loadClass("net.minecraft.resources.ResourceLocation")
+
+StartupEvents.registry("item", event => {
+
+    let p = ItemClass.Properties().craftRemainder(Items.BOWL).stacksTo(1);
+    let f = () => BuiltInRegistries.FLUID.get(ResourceLocation.fromNamespaceAndPath("kubejs", "brew"))
+
+    event.createCustom("brew", () => new StewItem(CPBlocks.BOWL.get(), f, p))
+})
