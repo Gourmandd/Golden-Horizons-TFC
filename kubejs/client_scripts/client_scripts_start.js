c5932240dae79e24b58f251de9aa8808717a4842
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-12-25 11:06:26 +0000

    Moved stuff around.

--- kubejs/client_scripts/client_scripts_start.js
+++ kubejs/client_scripts/client_scripts_start.js
@@ -1,8 +1,14 @@
 // priority: 100
-
+// Visit the wiki for more info - https://kubejs.com/
 
 let mod_id = "modpack"
 
-// Visit the wiki for more info - https://kubejs.com/
+function nameProcessing(name) {
+    name = name.replace("_", " ")
+    name = name.replace("_", " ")
+    name = name.replace(/(^\w|\s\w)/g, m => m.toUpperCase())
+    //console.log(name)
+    return name
+}
 
 console.info('Hello, World! (Loaded client scripts)')
