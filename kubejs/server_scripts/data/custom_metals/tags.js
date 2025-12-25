9a76937dc24b75f633e644303c3fbd5afa65c66c
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2025-12-25 15:44:17 +0000

    Added tags for custom metal block and items.

---
+++ kubejs/server_scripts/data/custom_metals/tags.js
@@ -0,0 +1,24 @@
+ServerEvents.tags("item", event => {
+
+    global.KUBEJS_METALS.forEach(metal => {
+        event.add("c:ingots", `${mod_id}:metal/ingot/${metal}`)
+        event.add("c:double_ingots", `${mod_id}:metal/double_ingot/${metal}`)
+        event.add("c:sheets", `${mod_id}:metal/sheet/${metal}`)
+        event.add("c:double_sheets", `${mod_id}:metal/double_sheet/${metal}`)
+        event.add("c:rods", `${mod_id}:metal/rod/${metal}`)
+
+        event.add("tfc:metal_plated_blocks", `${mod_id}:metal/block/${metal}`)
+        event.add("minecraft:stairs", `${mod_id}:metal/block/${metal}_stairs`)
+        event.add("minecraft:slabs", `${mod_id}:metal/block/${metal}_slab`)
+    })
+})
+
+ServerEvents.tags("block", event => {
+
+    global.KUBEJS_METALS.forEach(metal => {
+        event.add("minecraft:stairs", `${mod_id}:metal/block/${metal}_stairs`)
+        event.add("minecraft:slabs", `${mod_id}:metal/block/${metal}_slab`)
+
+        event.add("minecraft:mineable/pickaxe", [`${mod_id}:metal/block/${metal}`, `${mod_id}:metal/block/${metal}_stairs`, `${mod_id}:metal/block/${metal}_slab`])
+    })
+})
