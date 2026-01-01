446793033e07062af2557be153c246b5fc381988
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-01-01 11:53:38 +0000

    not sure what was happening there

--- kubejs/startup_scripts/constants/constants.js
+++ kubejs/startup_scripts/constants/constants.js
@@ -5,8 +5,6 @@
 // --------------------------------------- //
 
 let $CategoryUtil = Java.loadClass("net.gourmand.GoldenHorizonsCore.registry.category.CategoryUtil")
-let $CoreMetals = Java.loadClass("net.gourmand.GoldenHorizonsCore.registry.category.CoreMetals")
-let $CoreRocks = Java.loadClass("net.gourmand.GoldenHorizonsCore.registry.category.CoreRocks")
 
 // should switch to using a global
 //global.nameProcessing = (name) =>
@@ -40,18 +38,14 @@ global.CUSTOM_PASTEL_ORES = $CategoryUtil.getPastelOreNames()
 
 global.CUSTOM_ROCK_TYPES = $CategoryUtil.getAllRockNames()
 
-global.CORE_METALS = $CoreMetals.MetalType
-
-global.CORE_ROCKS =
-
-    global.METAL_FLUIDS = {
-        "aluminium": id + ":aluminium",
-        "aluminium_bronze": "createbigcannons:molten_bronze",
-        "hardened_steel": "createbigcannons:molten_steel",
-        "cast_iron_alloy": "createbigcannons:molten_cast_iron",
-        "nethersteel": "createbigcannons:molten_nethersteel",
-        "electrum": id + ":electrum"
-    }
+global.METAL_FLUIDS = {
+    "aluminium": id + ":aluminium",
+    "aluminium_bronze": "createbigcannons:molten_bronze",
+    "hardened_steel": "createbigcannons:molten_steel",
+    "cast_iron_alloy": "createbigcannons:molten_cast_iron",
+    "nethersteel": "createbigcannons:molten_nethersteel",
+    "electrum": id + ":electrum"
+}
 
 global.CROPS = [
     "barley",
