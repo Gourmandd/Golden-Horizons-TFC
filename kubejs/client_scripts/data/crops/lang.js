692b42b644bd1ff7e2896a128b1762865c8764e6
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-01-23 20:15:24 +0000

    Added bush lang.

--- kubejs/client_scripts/data/crops/lang.js
+++ kubejs/client_scripts/data/crops/lang.js
@@ -9,8 +9,17 @@ ClientEvents.lang("en_us", event => {
     }
 
     global.CUSTOM_CROP_TYPES.forEach(crop => {
-        event.add(`item.modpack.seeds.${crop}`, nameProcessing(crop) + " Seeds")
-        event.add(`block.modpack.wild_crop.${crop}`, "Wild " + nameProcessing(crop))
-        event.add(`block.modpack.dead_crop.${crop}`, "Dead " + nameProcessing(crop))
+        event.add(`item.${mod_id}.seeds.${crop}`, nameProcessing(crop) + " Seeds")
+        event.add(`block.${mod_id}.wild_crop.${crop}`, "Wild " + nameProcessing(crop))
+        event.add(`block.${mod_id}.dead_crop.${crop}`, "Dead " + nameProcessing(crop))
+    })
+
+    global.CUSTOM_STATIONARY_BUSH_TYPES.forEach(bush => {
+        console.log(bush)
+        event.add(`block.${mod_id}.plant.${bush}_bush`, nameProcessing(bush) + " Bush")
+    })
+
+    global.CUSTOM_SPREADING_BUSH_TYPES.forEach(bush => {
+        event.add(`block.${mod_id}.plant.${bush}_bush`, nameProcessing(bush) + " Bush")
     })
 })
