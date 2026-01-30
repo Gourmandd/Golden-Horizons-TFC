128381c1458454da1d35630955d974c4e122ad77
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-01-30 19:46:53 +0000

    Update build-modpack.py

--- build-modpack.py
+++ build-modpack.py
@@ -13,7 +13,7 @@ pakku_command: str = "pakku"
 # Or run "python ./build-modpack.py --pakku-location={path/to/pakku.jar}" and replace "{path/to/pakku.jar}" with where your pakku.jar is downloaded.
 # if you don't specify a file location ("python ./build-modpack.py --pakku-location=") it will default to "~/Downloads/pakku.jar" which 
 
-# This script aims to run on any OS, make an Github issue if this is not the case.
+# This script aims to run on any OS, since its being tested on a Linux system. Please make an Github issue if this is not the case.
 # Do not run in your game instance folder, clone the repo to its own folder and then run this in that folder. (this script overrides user configs)
 # You may need a Curseforge API key. (see the Pakku documentation)
 
@@ -32,21 +32,6 @@ if type(args.pakku_location) is str:
 
 
 print("Step: Editing settings.")
-
-
-def edit_loaders(loaders: dict = {"forge":"47.3.6"}):
-
-    pakku_lock_file = open('pakku-lock.json', 'r+')
-    pakku_lock = json.loads(pakku_lock_file.read())
-    pakku_lock["loaders"] = loaders
-
-    pakku_lock_file.close()
-    open('pakku-lock.json', 'w').close()
-
-    pakku_lock_file = open('pakku-lock.json', 'r+')
-    json.dump(pakku_lock, pakku_lock_file, indent=4)
-    pakku_lock_file.close()
-
 
 # get modpack settings as dict.
 settings_file = open('modpack-settings.json', 'r')
@@ -76,24 +61,12 @@ for file in glob.iglob(FOLDER + "overrides/" + '**/*', recursive= True):
             shutil.copyfile(file, file.replace("overrides/", ""))
             files_to_remove.append(file.replace("overrides/", ""))
 
-
-# change loader to only Forge, to circumvent a Pakku bug.
-edit_loaders({"forge":"47.3.6"})
-
-
 # change modpack version in Pakku.
 os.system(pakku_command + " cfg -v " + settings["version"])
-
 
 print("Step: Exporting modpack.")
 # Export Curseforge, Modrinth and a server pack. 
 os.system(pakku_command + " export") 
-
-
-
-print("Step: Reverting changes")
-# change loader back to Fabric & Forge, to circumvent a Pakku bug.
-edit_loaders({"fabric": "0.16.14","forge": "47.3.6"})
 
 for file in files_to_remove:
     os.remove(file)
