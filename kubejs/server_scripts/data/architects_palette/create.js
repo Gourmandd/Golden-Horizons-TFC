64918c2de5d3288620ecc98833acd5f20e7e4be6
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-02-08 10:54:57 +0000

    Replaced some misc uses for mossy bricks.

--- kubejs/server_scripts/data/architects_palette/create.js
+++ kubejs/server_scripts/data/architects_palette/create.js
@@ -1,20 +1,21 @@
 // requires: architects_palette
 
-ServerEvents.recipes(event =>{
+ServerEvents.recipes(event => {
 
     let datagen = Datagen(event).recipe()
 
+
     const HEATED_COMPACTING = {
         "minecraft:stone_bricks": "architects_palette:heavy_stone_bricks",
-        "minecraft:mossy_stone_bricks": "architects_palette:heavy_mossy_stone_bricks",
+        "modpack:rock/mossy_bricks/argillite": "architects_palette:heavy_mossy_stone_bricks",
         "minecraft:end_stone": "architects_palette:heavy_end_stone_bricks",
         "minecraft:calcite": "architects_palette:heavy_calcite_bricks",
         "tfc:rock/raw/tuff": "architects_palette:heavy_tuff_bricks",
         "minecraft:dripstone_block": "architects_palette:heavy_dripstone_bricks"
     }
 
-    Object.keys(HEATED_COMPACTING).forEach(entry =>{
-        datagen.createCompacting(outputOf(IO_TYPE.ITEM, HEATED_COMPACTING[entry],1 ,1), inputOf(IO_TYPE.ITEM, entry, 1)).heatRequirement(HEAT_REQUIREMENT.HEATED)
+    Object.keys(HEATED_COMPACTING).forEach(entry => {
+        datagen.createCompacting(outputOf(IO_TYPE.ITEM, HEATED_COMPACTING[entry], 1, 1), inputOf(IO_TYPE.ITEM, entry, 1)).heatRequirement(HEAT_REQUIREMENT.HEATED)
             .id(`modpack:compacting/heavy_bricks/${entry.split(":").pop()}`)
             .generate()
     })
