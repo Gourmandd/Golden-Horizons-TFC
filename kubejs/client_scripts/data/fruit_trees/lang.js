4329793acbf7573a52013c54e2dfdd9c72ce454a
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-02-27 19:19:16 +0000

    Initial lang datagen port to mod.

--- kubejs/client_scripts/data/fruit_trees/lang.js
+++
@@ -1,9 +0,0 @@
-ClientEvents.lang("en_us", event => {
-
-    global.CUSTOM_FRUIT_TREE_TYPES.forEach(tree => {
-        event.add(`block.modpack.plant.${tree}_leaves`, nameProcessing(tree) + " Leaves")
-        event.add(`block.modpack.plant.${tree}_growing_branch`, nameProcessing(tree) + " Growing Branch")
-        event.add(`block.modpack.plant.${tree}_branch`, nameProcessing(tree) + " Branch")
-        event.add(`block.modpack.plant.${tree}_sapling`, nameProcessing(tree) + " Sapling")
-    })
-})
--- kubejs/client_scripts/data/glass/lang.js
+++
@@ -1,6 +0,0 @@
-ClientEvents.lang("en_us", event => {
-
-    global.DYE_COLOURS.forEach(colour => {
-        event.add(`fluid_type.modpack.glass.${colour}`, "Molten " + nameProcessing(colour) + " Glass")
-    })
-})
