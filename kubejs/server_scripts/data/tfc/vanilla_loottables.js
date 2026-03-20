c0b969c2d79035401a3f3ddf1ed25c612a1f2bff
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-03-20 19:11:16 +0000

    Fix glass loot tables.

--- kubejs/server_scripts/data/tfc/vanilla_loottables.js
+++ kubejs/server_scripts/data/tfc/vanilla_loottables.js
@@ -2,66 +2,68 @@
 // Bulk loottable changes //
 // ---------------------- //
 
-try{
-console.info("Loot Tables loaded")
+try {
+    console.info("Loot Tables loaded")
 
 
-LootJS.modifiers((event) => {
+    LootJS.modifiers((event) => {
 
-    const REMOVE_DIRT = [ //according to the logs this is somehow being redeclared. So its not a const for now.
-        "minecraft:dirt", // maybe making this local to the event could work
-        "minecraft:grass_block",
-        "minecraft:podzol",
-        "minecraft:mycelium",
-        "minecraft:farmland"
-    ]
+        const REMOVE_DIRT = [ //according to the logs this is somehow being redeclared. So its not a const for now.
+            "minecraft:dirt", // maybe making this local to the event could work
+            "minecraft:grass_block",
+            "minecraft:podzol",
+            "minecraft:mycelium",
+            "minecraft:farmland"
+        ]
 
-    const CORAL_TYPES = [
-        "fire",
-        "bubble",
-        "tube",
-        "brain",
-        "horn"
-    ]
+        const CORAL_TYPES = [
+            "fire",
+            "bubble",
+            "tube",
+            "brain",
+            "horn"
+        ]
 
-    const REMOVE_SEEDS = [
-        "minecraft:grass",
-        "minecraft:tall_grass",
-        "minecraft:fern",
-        "minecraft:large_fern"
-    ]
+        const REMOVE_SEEDS = [
+            "minecraft:grass",
+            "minecraft:tall_grass",
+            "minecraft:fern",
+            "minecraft:large_fern"
+        ]
 
 
-    global.DYE_COLOURS.forEach(colour => { 
-        event.addBlockModifier(`minecraft:${colour}_stained_glass`).addLoot(`minecraft:${colour}_stained_glass`)
-        // hnh is not on 1.21, to be replaced probably.
-        //event.addBlockLootModifier(`hearth_and_home:${colour}_stained_barred_glass`).addLoot(`hearth_and_home:${colour}_stained_barred_glass`).removeLoot("minecraft:iron_nugget")
-        //event.addBlockLootModifier(`hearth_and_home:${colour}_stained_barred_glass_pane`).addLoot(`hearth_and_home:${colour}_stained_barred_glass_pane`).removeLoot("minecraft:iron_nugget")
+        event.addBlockModifier(`minecraft:glass`).removeLoot(`quark:clear_shard`)
+        global.DYE_COLOURS.forEach(colour => {
+            event.addBlockModifier(`minecraft:${colour}_stained_glass`)
+                .removeLoot(`minecraft:${colour}_stained_glass`)
+                .removeLoot(`quark:${colour}_shard`)
+                .addAlternativesLoot(
+                    LootEntry.of(`minecraft:${colour}_stained_glass`).matchTool(
+                        ItemFilter.item(Item.of("tfc:gem_saw"), false).or(ItemFilter.not(ItemFilter.hasEnchantment("minecraft:silk_touch", 1)))
+                    ),
+                )
+        })
+
+        CORAL_TYPES.forEach(type => {
+            event.addBlockModifier(`minecraft:${type}_coral`).removeLoot(`minecraft:${type}_coral`).addLoot(`tfc:coral/${type}_coral`)
+            event.addBlockModifier(`minecraft:dead_${type}_coral`).removeLoot(`minecraft:dead_${type}_coral`).addLoot(`tfc:coral/${type}_dead_coral`)
+            event.addBlockModifier(`minecraft:${type}_coral_fan`).removeLoot(`minecraft:${type}_coral_fan`).addLoot(`tfc:coral/${type}_coral_fan`)
+            event.addBlockModifier(`minecraft:dead_${type}_coral_fan`).removeLoot(`minecraft:dead_${type}_coral_fan`).addLoot(`tfc:coral/${type}_dead_coral_fan`)
+        })
+
+        REMOVE_DIRT.forEach(blockID => {
+            event.addBlockModifier(blockID).replaceLoot("minecraft:dirt", "tfc:dirt/aridisol")
+        })
+
+        REMOVE_SEEDS.forEach(blockID => {
+            event.addBlockModifier(blockID).removeLoot("minecraft:wheat_seeds")
+        })
+
+        //minecraft glass is hardcoded to not drop anything unless gem saw or silk touch??
+        //event.addBlockLootModifier("minecraft:orange_stained_glass").addLoot("minecraft:orange_stained_glass")
+        //event.addBlockLootModifier("minecraft:glass").addLoot("minecraft:glass")
     })
 
-    CORAL_TYPES.forEach(type =>{
-        event.addBlockModifier(`minecraft:${type}_coral`).removeLoot(`minecraft:${type}_coral`).addLoot(`tfc:coral/${type}_coral`)
-        event.addBlockModifier(`minecraft:dead_${type}_coral`).removeLoot(`minecraft:dead_${type}_coral`).addLoot(`tfc:coral/${type}_dead_coral`)
-        event.addBlockModifier(`minecraft:${type}_coral_fan`).removeLoot(`minecraft:${type}_coral_fan`).addLoot(`tfc:coral/${type}_coral_fan`)
-        event.addBlockModifier(`minecraft:dead_${type}_coral_fan`).removeLoot(`minecraft:dead_${type}_coral_fan`).addLoot(`tfc:coral/${type}_dead_coral_fan`)
-    })
-
-    REMOVE_DIRT.forEach(blockID =>{
-        event.addBlockModifier(blockID).replaceLoot("minecraft:dirt", "tfc:dirt/aridisol")
-    })
-
-    REMOVE_SEEDS.forEach(blockID => {
-        event.addBlockModifier(blockID).removeLoot("minecraft:wheat_seeds")
-    })
-
-    //minecraft glass is hardcoded to not drop anything unless gem saw or silk touch??
-    //event.addBlockLootModifier("minecraft:orange_stained_glass").addLoot("minecraft:orange_stained_glass")
-    //event.addBlockLootModifier("minecraft:glass").addLoot("minecraft:glass")
-    //hearth and home
-    //event.addBlockLootModifier("hearth_and_home:barred_glass").addLoot("hearth_and_home:barred_glass").removeLoot("minecraft:iron_nugget")
-    //event.addBlockLootModifier("hearth_and_home:barred_glass_pane").addLoot("hearth_and_home:barred_glass_pane").removeLoot("minecraft:iron_nugget")
-})
-
-} catch(e) {
+} catch (e) {
     console.log(e)
 }
