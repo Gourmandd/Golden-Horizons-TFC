993613c3cc3aae867dae28e1cb635797b4f7e1ea
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-06-13 11:20:19 +0100

    Removed crushed terracotta.

--- kubejs/startup_scripts/registry/assorted.js
+++ kubejs/startup_scripts/registry/assorted.js
@@ -13,12 +13,6 @@ StartupEvents.registry("item", event => {
     event.create(`${id}:olive_pomace`).displayName("Olive Pomace")
     event.create(`${id}:dry_olive_pomace`).displayName("Dry Olive Pomace")
 
-
-    // to be ported to the mod.
-    global.DYE_COLOURS.forEach(colour => {
-        event.create(`${id}:terracotta/shard_${colour}`).displayName(`${nameProcessing(colour)} Terracotta Shard`)
-    })
-
     event.create(`${id}:raw_aluminium_mixture`).displayName("Raw Aluminium Mixture")
     event.create(`${id}:aluminium_nugget`).displayName("Aluminium Nugget")
 })
@@ -41,20 +35,6 @@ StartupEvents.registry("block", event => {
         .displayName("Snow Brick Stairs")
         .tagBlock("minecraft:mineable/pickaxe")
         .tagBlock("minecraft:mineable/shovel")
-
-    // to be ported to the mod.
-    global.DYE_COLOURS.forEach(colour => {
-        event.create(`${id}:terracotta/crushed_${colour}`)
-            .displayName(`${nameProcessing(colour)} Crushed Terracotta`)
-            .soundType("sand")
-            .requiresTool(true)
-            .tagBlock("minecraft:mineable/pickaxe")
-            .tagBlock("minecraft:mineable/shovel")
-            .tagBlock("tfc:can_landslide")
-            .tagBlock("minecraft:sand")
-            .tagBlock("tfc:grass_plantable")
-    })
-
 
     event.create(`${id}:andesite_machine`).displayName("Andesite Machine").tagBlock("minecraft:mineable/pickaxe").tagBlock("minecraft:mineable/axe")
     event.create(`${id}:copper_machine`).displayName("Copper Machine").tagBlock("minecraft:mineable/pickaxe").tagBlock("minecraft:mineable/axe")
