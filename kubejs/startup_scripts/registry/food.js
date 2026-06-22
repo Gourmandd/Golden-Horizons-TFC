26142133a06952e73f00750d671c0c4134333e47
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-06-22 13:38:00 +0100

    WIP port from Farmers Delight TFC to Survivors Delight, no pakku work done in here.

--- kubejs/startup_scripts/registry/food.js
+++ kubejs/startup_scripts/registry/food.js
@@ -13,4 +13,11 @@ StartupEvents.registry("item", event => {
     event.create(`${id}:hard_chhurpi`).displayName("Hard Chhurpi").food(0, 0)
 
     event.create(`${id}:food/amaranth_flour`).displayName("Amaranth Flour").food(0, 0)
+
+    event.create(`${id}:food/myceylon_cookie_dough`).displayName("Myceylon Cookie Dough").food(0, 0)
+    event.create(`${id}:food/sweet_berry_cookie_dough`).displayName("Sweet Berry Cookie Dough").food(0, 0)
+    event.create(`${id}:food/honey_cookie_dough`).displayName("Honey Cookie Dough").food(0, 0)
+    event.create(`${id}:food/syrup_cookie_dough`).displayName("Sweet Syrup Cookie Dough").food(0, 0)
+    event.create(`${id}:food/cherry_blossom_cookie_dough`).displayName("Cherry Blossom Cookie Dough").food(0, 0)
+    event.create(`${id}:food/coffee_cookie_dough`).displayName("Coffee Cookie Dough").food(0, 0)
 })
